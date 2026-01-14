1d38e84d461b322ab397e900eadfdf05bde5600e
Meghraj Singh <117947064+meghrajs@users.noreply.github.com>
2026-01-14 16:52:14 -0500

    Add end-to-end API to UI validation test

---
+++ tests/e2e/ApiToUiTest.java
@@ -0,0 +1,29 @@
+package tests.e2e;
+
+import framework.base.BaseTest;
+import framework.config.Config;
+import framework.pages.PlaywrightHomePage;
+import io.restassured.RestAssured;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiToUiTest extends BaseTest {
+
+  @Test(groups = {"smoke", "e2e"})
+  public void apiSeedThenUiValidation() {
+    RestAssured.baseURI = Config.apiBaseUrl();
+
+    var res = given().when().get("/api/users/2").then().extract().response();
+    Assert.assertTrue(res.statusCode() >= 200 && res.statusCode() < 300);
+
+    String firstName = res.jsonPath().getString("data.first_name");
+    Assert.assertNotNull(firstName);
+
+    PlaywrightHomePage home = new PlaywrightHomePage(driver);
+    home.open(baseUrl());
+    home.clickDocs();
+    Assert.assertTrue(driver.getCurrentUrl().contains("docs"));
+  }
+}
