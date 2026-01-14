74d9442c723b511ea4021fd85a4bf3edbcb61ef3
Meghraj Singh <117947064+meghrajs@users.noreply.github.com>
2026-01-14 17:02:40 -0500

    Refactor BaseTest for improved WebDriver management
    Refactor BaseTest to manage WebDriver through DriverManager and capture screenshots on test failure.

--- framework/base/BaseTest.java
+++ framework/base/BaseTest.java
@@ -1,22 +1,36 @@
 package framework.base;
 
 import framework.config.Config;
+import framework.utils.ScreenshotUtil;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-  protected WebDriver driver;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
-    driver = DriverFactory.create();
-    driver.manage().window().maximize();
+    WebDriver driver = DriverFactory.create();
+    DriverManager.set(driver);
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() {
-    if (driver != null) driver.quit();
+  public void tearDown(ITestResult result) {
+    try {
+      if (!result.isSuccess() && DriverManager.get() != null) {
+        ScreenshotUtil.capture(DriverManager.get(), result.getName());
+      }
+    } finally {
+      if (DriverManager.get() != null) {
+        DriverManager.get().quit();
+      }
+      DriverManager.unload();
+    }
+  }
+
+  protected WebDriver driver() {
+    return DriverManager.get();
   }
 
   protected String baseUrl() {
