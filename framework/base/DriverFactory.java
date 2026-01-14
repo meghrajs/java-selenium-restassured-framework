6885191f4fd6febedaeaab37bdadff2254f7cb45
Meghraj Singh <117947064+meghrajs@users.noreply.github.com>
2026-01-14 17:02:17 -0500

    Integrate WebDriverManager for ChromeDriver setup

--- framework/base/DriverFactory.java
+++ framework/base/DriverFactory.java
@@ -1,16 +1,22 @@
 package framework.base;
 
 import framework.config.Config;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
+
   public static WebDriver create() {
+    WebDriverManager.chromedriver().setup();
+
     ChromeOptions options = new ChromeOptions();
     if (Config.headless()) options.addArguments("--headless=new");
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--window-size=1920,1080");
+
     return new ChromeDriver(options);
   }
 }
